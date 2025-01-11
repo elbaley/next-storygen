9e50fa086de4e078758910300537ecf360174461
elbaley <lebafurkan@outlook.com>
2025-01-11 14:57:05 +0300

    feat: story editor  add multiple stories

--- src/app/conva/page.tsx
+++ src/app/conva/page.tsx
@@ -3,14 +3,86 @@
 import { Stage, Layer, Text, Group, Label, Tag } from "react-konva";
 import { BASE_HEIGHT, BASE_WIDTH } from "@/constants";
 import { useState, useRef } from "react";
-import { Baseline, Menu, Save, Trash, Type } from "lucide-react";
+import { Baseline, LucideShuffle, Menu, Save, Trash, Type } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
-import { Popover } from "@/components/ui/popover";
-import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Page() {
+  const [stories, setStories] = useState([
+    {
+      id: 1,
+      backgroundImage: "https://picsum.photos/id/0/1080/1920",
+      texts: [
+        {
+          id: 1,
+          text: "Hello World",
+          x: 0,
+          y: 0,
+          fontSize: 24,
+          background: "black",
+          color: "white",
+        },
+        {
+          id: 2,
+          text: "Story 1 Text",
+          x: 100,
+          y: BASE_HEIGHT - 35,
+          fontSize: 18,
+          background: "black",
+          color: "white",
+        },
+      ],
+    },
+    {
+      id: 2,
+      background: "black",
+      texts: [
+        {
+          id: 1,
+          text: "Hello World",
+          x: 0,
+          y: 0,
+          fontSize: 24,
+          background: "black",
+          color: "white",
+        },
+        {
+          id: 2,
+          text: "Story 1 Text",
+          x: 100,
+          y: BASE_HEIGHT - 35,
+          fontSize: 18,
+          background: "black",
+          color: "white",
+        },
+      ],
+    },
+  ]);
+
+  const [activeStoryIndex, setActiveStoryIndex] = useState(0);
+  const currentStory = stories[activeStoryIndex];
+  const [editingId, setEditingId] = useState<number | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleAddStory = () => {
+    setStories([
+      ...stories,
+      {
+        id: Date.now(),
+        background: "white",
+        texts: [],
+      },
+    ]);
+    setActiveStoryIndex(stories.length); // Set the new story as active
+  };
+
   const backgroundPresets = [
     {
       id: 0,
@@ -31,65 +103,73 @@ export default function Page() {
 
   const handleCyclePreset = () => {
     if (editingId !== null) {
-      setTexts((prevTexts) =>
-        prevTexts.map((text) => {
-          if (text.id === editingId) {
-            const currentPresetIndex = backgroundPresets.findIndex(
-              (preset) =>
-                preset.background === text.background &&
-                preset.color === text.color,
-            );
-            const nextPresetIndex =
-              (currentPresetIndex + 1) % backgroundPresets.length;
+      setStories((prevStories) => {
+        return prevStories.map((story, index) => {
+          if (index === activeStoryIndex) {
             return {
-              ...text,
-              background: backgroundPresets[nextPresetIndex].background,
-              color: backgroundPresets[nextPresetIndex].color,
+              ...story,
+              texts: story.texts.map((text) => {
+                if (text.id === editingId) {
+                  const currentPresetIndex = backgroundPresets.findIndex(
+                    (preset) =>
+                      preset.background === text.background &&
+                      preset.color === text.color,
+                  );
+                  const nextPresetIndex =
+                    (currentPresetIndex + 1) % backgroundPresets.length;
+                  return {
+                    ...text,
+                    background: backgroundPresets[nextPresetIndex].background,
+                    color: backgroundPresets[nextPresetIndex].color,
+                  };
+                }
+                return text;
+              }),
             };
           }
-          return text;
-        }),
-      );
+          return story;
+        });
+      });
     }
   };
 
-  const [texts, setTexts] = useState([
-    {
-      id: 1,
-      text: "Hello World",
-      x: 0,
-      y: 0,
-      fontSize: 24,
-      background: "black",
-      color: "white",
-    },
-    {
-      id: 2,
-      text: "Konva Text",
-      x: 100,
-      y: BASE_HEIGHT - 35,
-      fontSize: 18,
-      background: "black",
-      color: "white",
-    },
-  ]);
-  const [editingId, setEditingId] = useState<number | null>(null);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const handleShuffleBackground = () => {
+    const randomId = Math.floor(Math.random() * 100);
+    const url = `https://picsum.photos/id/${randomId}/1080/1920`;
+
+    setStories((prevStories) => {
+      return prevStories.map((story, index) => {
+        if (index === activeStoryIndex) {
+          return {
+            ...story,
+            backgroundImage: url,
+            background: undefined,
+          };
+        }
+        return story;
+      });
+    });
+  };
 
   const handleAddText = () => {
     const id = Date.now();
-    setTexts([
-      ...texts,
-      {
-        id,
-        text: "Text",
-        x: BASE_WIDTH / 2.5,
-        y: BASE_HEIGHT / 2,
-        fontSize: 20,
-        background: "black",
-        color: "white",
-      },
-    ]);
+    const newText = {
+      id,
+      text: "Text",
+      x: BASE_WIDTH / 2.5,
+      y: BASE_HEIGHT / 2,
+      fontSize: 20,
+      background: "black",
+      color: "white",
+    };
+
+    setStories(
+      stories.map((story, index) =>
+        index === activeStoryIndex
+          ? { ...story, texts: [...story.texts, newText] }
+          : story,
+      ),
+    );
 
     setEditingId(id);
     if (inputRef.current) {
@@ -102,7 +182,7 @@ export default function Page() {
 
   const handleEditText = (id: number) => {
     setEditingId(id);
-    const text = texts.find((t) => t.id === id);
+    const text = currentStory.texts.find((t) => t.id === id);
     if (text && inputRef.current) {
       inputRef.current.value = text.text;
       inputRef.current.focus();
@@ -116,43 +196,78 @@ export default function Page() {
   const handleUpdateText = () => {
     if (editingId !== null && inputRef.current) {
       const newText = inputRef.current.value;
-      setTexts(
-        texts.map((t) => (t.id === editingId ? { ...t, text: newText } : t)),
+      setStories(
+        stories.map((story, index) =>
+          index === activeStoryIndex
+            ? {
+                ...story,
+                texts: story.texts.map((t) =>
+                  t.id === editingId ? { ...t, text: newText } : t,
+                ),
+              }
+            : story,
+        ),
       );
+
       setEditingId(null);
     }
   };
 
   const handleDeleteText = () => {
     if (editingId !== null) {
-      setTexts(texts.filter((t) => t.id !== editingId));
+      setStories(
+        stories.map((story, index) =>
+          index === activeStoryIndex
+            ? {
+                ...story,
+                texts: story.texts.filter((t) => t.id !== editingId),
+              }
+            : story,
+        ),
+      );
       setEditingId(null);
     }
   };
 
   return (
     <main className="w-full pt-4">
-      <Popover>
-        <PopoverTrigger asChild>
-          <button className="bg-zinc-200 ml-4 hover:bg-orange-200 p-3 rounded-sm ">
-            <Menu size={16} />
+      <div className="w-full flex items-center gap-4 justify-center">
+        {/* Sidebar for story selection */}
+        <div>
+          <ScrollArea className="h-64 w-12">
+            <div className="space-y-2">
+              {stories.map((story, index) => (
+                <button
+                  key={story.id}
+                  onClick={() => setActiveStoryIndex(index)}
+                  className={cn(
+                    "w-12 h-12 border text-center shrink-0",
+                    activeStoryIndex === index ? "bg-blue-200" : "bg-gray-100",
+                  )}
+                >
+                  {index + 1}
+                </button>
+              ))}
+            </div>
+          </ScrollArea>
+          <button
+            onClick={handleAddStory}
+            className="w-12 h-12 bg-green-100 mt-4 hover:bg-green-200"
+          >
+            +
           </button>
-        </PopoverTrigger>
-        <PopoverContent
-          side="bottom"
-          className="rounded-sm p-2 ml-4 mt-2 shadow-[0_0_0.931px_0_rgba(0,0,0,0.17),0_0_3.127px_0_rgba(0,0,0,0.08),0_7px_14px_0_rgba(0,0,0,0.05)]"
-        >
-          <div>Background</div>
-        </PopoverContent>
-      </Popover>
-
-      <div className="w-full flex items-center justify-center">
+        </div>
         <div
           className="bg-cover relative border border-black"
           style={{
             width: `${BASE_WIDTH}px`,
             height: `${BASE_HEIGHT}px`,
-            backgroundImage: "url(https://picsum.photos/id/0/1080/1920)",
+            ...(currentStory.background && {
+              background: currentStory.background,
+            }),
+            ...(currentStory.backgroundImage && {
+              backgroundImage: `url(${currentStory.backgroundImage})`,
+            }),
           }}
         >
           <Stage
@@ -165,7 +280,7 @@ export default function Page() {
             height={BASE_HEIGHT}
           >
             <Layer>
-              {texts.map((t) => (
+              {currentStory.texts.map((t) => (
                 <Group
                   key={t.id}
                   x={t.x}
@@ -173,15 +288,16 @@ export default function Page() {
                   draggable
                   onDragEnd={(e) => {
                     const { x, y } = e.currentTarget.getAbsolutePosition();
-                    setTexts(
-                      texts.map((text) =>
-                        text.id === t.id
+                    setStories(
+                      stories.map((story, index) =>
+                        index === activeStoryIndex
                           ? {
-                              ...text,
-                              x,
-                              y,
+                              ...story,
+                              texts: story.texts.map((text) =>
+                                text.id === t.id ? { ...text, x, y } : text,
+                              ),
                             }
-                          : text,
+                          : story,
                       ),
                     );
                   }}
@@ -207,41 +323,65 @@ export default function Page() {
           </Stage>
         </div>
       </div>
-
-      <div className="flex justify-center w-full pt-4 gap-4 items-center">
-        <div className="py-2 px-3  shadow-[0_0_0.931px_0_rgba(0,0,0,0.17),0_0_3.127px_0_rgba(0,0,0,0.08),0_7px_14px_0_rgba(0,0,0,0.05)] rounded-lg min-w-96 flex gap-2">
-          <button
-            disabled={editingId !== null}
-            onClick={handleAddText}
-            className="hover:bg-orange-200 p-2 rounded-md"
+      <div className="flex justify-center items-center pt-4 gap-2">
+        <Popover>
+          <PopoverTrigger asChild>
+            <button className="bg-purple-100  hover:bg-purple-200 p-4 rounded-sm ">
+              <Menu size={16} />
+            </button>
+          </PopoverTrigger>
+          <PopoverContent
+            side="top"
+            className="bg-white rounded-sm p-2 ml-4 mb-2 shadow-[0_0_0.931px_0_rgba(0,0,0,0.17),0_0_3.127px_0_rgba(0,0,0,0.08),0_7px_14px_0_rgba(0,0,0,0.05)]"
           >
-            <Type size={16} />
-          </button>
-          <Separator orientation="vertical" />
-          <div className={cn("flex gap-2", editingId === null && "hidden")}>
+            <span className="text-sm">Background</span>
+            <div className="flex gap-2">
+              <button
+                onClick={handleShuffleBackground}
+                className="flex justify-center items-center h-6 aspect-square rounded-sm border hover:opacity-70"
+              >
+                <LucideShuffle size={16} strokeWidth={1.5} />
+              </button>
+            </div>
+          </PopoverContent>
+        </Popover>
+
+        {/* Toolbar */}
+        <div className="flex justify-center wfull  gap-4 items-center">
+          <div className="py-2 px-3  shadow-[0_0_0.931px_0_rgba(0,0,0,0.17),0_0_3.127px_0_rgba(0,0,0,0.08),0_7px_14px_0_rgba(0,0,0,0.05)] rounded-lg min-w-96 flex gap-2">
             <button
-              onClick={handleCyclePreset}
-              className={cn(
-                "p-2 rounded-md",
-                texts.find((t) => t.id === editingId)?.background !==
-                  "transparent" && "outline outline-black ",
-              )}
-            >
-              <Baseline size={16} />
-            </button>
-            <Input tabIndex={0} className="h-8" ref={inputRef} />
-            <button
-              onClick={handleUpdateText}
+              disabled={editingId !== null}
+              onClick={handleAddText}
               className="hover:bg-orange-200 p-2 rounded-md"
             >
-              <Save size={16} />
+              <Type size={16} />
             </button>
-            <button
-              onClick={handleDeleteText}
-              className="hover:bg-orange-200 p-2 rounded-md"
-            >
-              <Trash size={16} />
-            </button>
+            <Separator orientation="vertical" />
+            <div className={cn("flex gap-2", editingId === null && "hidden")}>
+              <button
+                onClick={handleCyclePreset}
+                className={cn(
+                  "p-2 rounded-md",
+                  currentStory.texts.find((t) => t.id === editingId)
+                    ?.background !== "transparent" && "outline outline-black ",
+                )}
+              >
+                <Baseline size={16} />
+              </button>
+              <Input tabIndex={0} className="h-8" ref={inputRef} />
+              <button
+                onClick={handleUpdateText}
+                className="hover:bg-orange-200 p-2 rounded-md"
+              >
+                <Save size={16} />
+              </button>
+              <button
+                onClick={handleDeleteText}
+                className="hover:bg-orange-200 p-2 rounded-md"
+              >
+                <Trash size={16} />
+              </button>
+            </div>
           </div>
         </div>
       </div>
