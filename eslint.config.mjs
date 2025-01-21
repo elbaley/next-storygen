fefb4373f9e35a80333d40468e74850f943b9c91
elbaley <lebafurkan@outlook.com>
2025-01-21 23:01:19 +0300

    chore: eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "no-unused-vars": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
     },
   },
 ];
--- src/components/site/StoryEditor/_components/StoryNavigation.tsx
+++ src/components/site/StoryEditor/_components/StoryNavigation.tsx
@@ -17,20 +17,36 @@ export const StoryNavigation = ({
 }: StoryNavigationProps) => {
   return (
     <div className="mx-auto max-w-[320px] flex gap-2 items-center">
-      <ScrollArea className="flex-1">
-        <div className="flex gap-2">
+      <ScrollArea className="flex-1 ">
+        <div className="flex gap-2 overflow-visible px-5 py-5 -translate-x-5">
           {stories.map((story, index) => (
             <button
               key={story.id}
               onClick={() => setActiveStoryIndex(index)}
               className={cn(
-                "w-12 h-12 border text-center shrink-0",
+                "group flex h-10 w-10 select-none items-center justify-center rounded-lg border border-zinc-100 bg-white leading-8 text-zinc-950 shadow-[0_-1px_0_0px_#d4d4d8_inset,0_0_0_1px_#f4f4f5_inset,0_0.5px_0_1.5px_#fff_inset] hover:bg-zinc-50 hover:via-zinc-900 hover:to-zinc-800 active:shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]",
                 activeStoryIndex === index
-                  ? "bg-blue-200 border-dashed border-muted"
-                  : "bg-gray-100",
+                  ? "shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]"
+                  : "",
               )}
+
+              // className={cn(
+              //   "w-12 h-12 rounded-sm  border-secondary bg-white border drop-shadow-lg",
+              //   // activeStoryIndex === index
+              //   //   ? "bg-blue-200 border-dashed border-muted"
+              //   //   : "bg-gray-100",
+              // )}
             >
-              {index + 1}
+              <span
+                className={cn(
+                  "flex items-center group-active:[transform:translate3d(0,1px,0)]",
+                  activeStoryIndex === index
+                    ? "[transform:translate3d(0,1px,0)]"
+                    : "",
+                )}
+              >
+                {index + 1}
+              </span>
             </button>
           ))}
         </div>
