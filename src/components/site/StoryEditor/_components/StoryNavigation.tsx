60acad8a82a0f192333f305ca0b13538cca6bf33
elbaley <lebafurkan@outlook.com>
2025-01-21 23:26:17 +0300

    styling: horizontal story navigation

--- src/components/site/StoryEditor/_components/StoryNavigation.tsx
+++ src/components/site/StoryEditor/_components/StoryNavigation.tsx
@@ -1,6 +1,8 @@
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Story } from "../StoryEditor.types";
 import { cn } from "@/lib/utils";
+import { LucidePlus } from "lucide-react";
+import { useEffect, useRef } from "react";
 
 type StoryNavigationProps = {
   stories: Story[];
@@ -15,33 +17,39 @@ export const StoryNavigation = ({
   setActiveStoryIndex,
   activeStoryIndex,
 }: StoryNavigationProps) => {
+  const newStoryButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (newStoryButtonRef.current) {
+      newStoryButtonRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "center",
+      });
+    }
+  }, [newStoryButtonRef, activeStoryIndex]);
+
   return (
     <div className="mx-auto max-w-[320px] flex gap-2 items-center">
-      <ScrollArea className="flex-1 ">
-        <div className="flex gap-2 overflow-visible px-5 py-5 -translate-x-5">
+      <ScrollArea type="scroll" className="flex-1 ">
+        <div className="flex gap-2  pl-5 py-3 -translate-x-5">
           {stories.map((story, index) => (
             <button
               key={story.id}
+              ref={index === activeStoryIndex ? newStoryButtonRef : null}
               onClick={() => setActiveStoryIndex(index)}
               className={cn(
                 "group flex h-10 w-10 select-none items-center justify-center rounded-lg border border-zinc-100 bg-white leading-8 text-zinc-950 shadow-[0_-1px_0_0px_#d4d4d8_inset,0_0_0_1px_#f4f4f5_inset,0_0.5px_0_1.5px_#fff_inset] hover:bg-zinc-50 hover:via-zinc-900 hover:to-zinc-800 active:shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]",
                 activeStoryIndex === index
-                  ? "shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]"
+                  ? "bg-zinc-100 via-zinc-900 to-zinc-800 hover:bg-zinc-100"
                   : "",
               )}
-
-              // className={cn(
-              //   "w-12 h-12 rounded-sm  border-secondary bg-white border drop-shadow-lg",
-              //   // activeStoryIndex === index
-              //   //   ? "bg-blue-200 border-dashed border-muted"
-              //   //   : "bg-gray-100",
-              // )}
             >
               <span
                 className={cn(
                   "flex items-center group-active:[transform:translate3d(0,1px,0)]",
                   activeStoryIndex === index
-                    ? "[transform:translate3d(0,1px,0)]"
+                    ? "[transform:translate3d(0,1px,0)] font-semibold "
                     : "",
                 )}
               >
@@ -54,9 +62,17 @@ export const StoryNavigation = ({
       </ScrollArea>
       <button
         onClick={handleAddStory}
-        className="w-12 h-12 bg-green-100  hover:bg-green-200"
+        className={cn(
+          "group flex h-10 w-10 select-none items-center justify-center rounded-lg border border-zinc-100 bg-white leading-8 text-zinc-950 shadow-[0_-1px_0_0px_#d4d4d8_inset,0_0_0_1px_#f4f4f5_inset,0_0.5px_0_1.5px_#fff_inset] hover:bg-zinc-50 hover:via-zinc-900 hover:to-zinc-800 active:shadow-[-1px_0px_1px_0px_#e4e4e7_inset,1px_0px_1px_0px_#e4e4e7_inset,0px_0.125rem_1px_0px_#d4d4d8_inset]",
+        )}
       >
-        +
+        <span
+          className={cn(
+            "flex items-center group-active:[transform:translate3d(0,1px,0)]",
+          )}
+        >
+          <LucidePlus size={16} strokeWidth={1.75} />
+        </span>
       </button>
     </div>
   );
